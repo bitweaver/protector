83eb49f5f84fbf59c8874f6b43cace43ff5dc3c7
Lester Caine <lester@lsces.co.uk>
2006-02-10 10:30:48 +0000

    Added content_preview_function to match content_edit_function, since the latter is not called when preview is selected, so the service is not set up correctly for edit at the bottom of the page.

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -7,6 +7,7 @@ require_once( PROTECTOR_PKG_PATH.'LibertyProtector.php' );
 if( $gBitSystem->isPackageActive( 'protector' ) ) {
 	$gLibertySystem->registerService( LIBERTY_SERVICE_ACCESS_CONTROL, PROTECTOR_PKG_NAME, array(
 		'content_display_function' => 'protector_content_display',
+		'content_preview_function' => 'protector_content_edit',
 		'content_edit_function' => 'protector_content_edit',
 		'content_store_function' => 'protector_content_store',
 		'content_list_function' => 'protector_content_list',
