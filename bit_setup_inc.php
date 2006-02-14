f9ed5711c078bb51a74e32d5fafa4ca29e1cb725
Max Kremmel <xing@synapse.plus.com>
2006-02-14 21:53:25 +0000

    rename content_load_function to content_load_sql_function

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -10,8 +10,8 @@ if( $gBitSystem->isPackageActive( 'protector' ) ) {
 		'content_preview_function' => 'protector_content_edit',
 		'content_edit_function' => 'protector_content_edit',
 		'content_store_function' => 'protector_content_store',
-		'content_list_function' => 'protector_content_list',
-		'content_load_function' => 'protector_content_load',
+		'content_list_sql_function' => 'protector_content_list',
+		'content_load_sql_function' => 'protector_content_load',
 		'content_edit_mini_tpl' => 'bitpackage:protector/choose_protection.tpl',
 		'content_icon_tpl' => 'bitpackage:protector/protector_service_icon.tpl',
 	) );
