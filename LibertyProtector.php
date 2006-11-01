43cbce901a642d2bb389aa1073476422490c0e52
Lester Caine <lester@lsces.co.uk>
2006-11-01 10:06:06 +0000

    Remove multiple entries from listings when multiple users assigned to group Not sure if this was just a Firebird problem, but JOIN was giving multiple coppies of content.

--- LibertyProtector.php
+++ LibertyProtector.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_protector/LibertyProtector.php,v 1.4 2006/03/01 21:11:44 lsces Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_protector/LibertyProtector.php,v 1.5 2006/11/01 10:06:06 lsces Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: LibertyProtector.php,v 1.4 2006/03/01 21:11:44 lsces Exp $
+ * $Id: LibertyProtector.php,v 1.5 2006/11/01 10:06:06 lsces Exp $
  * @package protector
  */
 
@@ -28,7 +28,7 @@ require_once( LIBERTY_PKG_PATH.'LibertyBase.php' );
  *
  * @author spider <spider@steelsun.com>
  *
- * @version $Revision: 1.4 $ $Date: 2006/03/01 21:11:44 $ $Author: lsces $
+ * @version $Revision: 1.5 $ $Date: 2006/11/01 10:06:06 $ $Author: lsces $
  */
 class LibertyProtector extends LibertyBase {
     /**
@@ -55,11 +55,11 @@ function protector_content_list() {
 	global $gBitUser;
 	$groups = array_keys($gBitUser->mGroups);
 	$ret = array(
-		'join_sql' => " LEFT JOIN `".BIT_DB_PREFIX."liberty_content_group_map` lcgm ON ( lc.`content_id`=lcgm.`content_id` ) LEFT OUTER JOIN `".BIT_DB_PREFIX."users_groups_map` ugm ON ( ugm.`group_id`=lcgm.`group_id` ) ",
+		'join_sql' => " LEFT JOIN `".BIT_DB_PREFIX."liberty_content_group_map` lcgm ON ( lc.`content_id`=lcgm.`content_id` ) LEFT OUTER JOIN `".BIT_DB_PREFIX."users_groups_map` ugm ON ( ugm.`user_id`=? ) AND ( ugm.`group_id`=lcgm.`group_id` ) ",
 		'where_sql' => " AND (lcgm.`content_id` IS NULL OR lcgm.`group_id` IN(". implode(',', array_fill(0, count($groups), '?')) ." ) OR ugm.`user_id`=?) ",
-		'bind_vars' => array( $gBitUser->mUserId ),
+		'bind_vars' => array_merge( array( $gBitUser->mUserId ), $groups, array( $gBitUser->mUserId ) ),
 	);
-	$ret['bind_vars'] = array_merge( $groups, $ret['bind_vars'] );
+//	$ret['bind_vars'] = array_merge( $groups, array( $gBitUser->mUserId ) );
 	return $ret;
 }
 
